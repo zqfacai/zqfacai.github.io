219fa21644459c30ecd61e59a363f2635b9ae650
zqfacai <167156382+zqfacai@users.noreply.github.com>
2024-04-16 11:17:12 +0800

    Create foot.js

---
+++ js/foot.js
@@ -0,0 +1,20 @@
+
+
+
+
+document.write('<div class=\"main-content new-footer clearfix\">');
+document.write('    <div class=\"footer-center\">');        
+document.write('        <div class=\"desc\">');
+document.write('            <div class=\"footcontact \">'); 
+document.write(' <p>  The information is collected from internet and the copyright belongs to the original author. If there is any infringement, please contact to delete. notrynogain@gmail.com ');
+document.write('<script type="text/javascript"  src="https://js.users.51.la/18779678.js"></script>   </p>');
+document.write('           </div>');           
+document.write('       </div>');
+document.write('   </div>');
+document.write('</div>');
+
+
+
+
+ 
+ 
